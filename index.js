ca216c931d15c29b6cd3fa232ef556ac7bf0dccb
gmat191@gmail.com <gmat191@gmail.com>
2018-06-09 03:43:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ var kittens = ['Milo', 'Otis', 'Garfield']
 
 function destructivelyAppendKitten(name) {
   kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.
 }
